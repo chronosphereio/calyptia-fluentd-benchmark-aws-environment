ed1f3738d2b45b5d466f8c9fdf332fd1d6b52d3a
Hiroshi Hatake <hatake@calyptia.com>
2021-05-12 19:04:07 +0900

    winevtlog_bench: Remove needless files
    Signed-off-by: Hiroshi Hatake <hatake@calyptia.com>

--- winevtlog_bench/config/settings.ps1
+++
@@ -1,3 +0,0 @@
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1 -OutFile ConfigureRemotingForAnsible.ps1
-powershell -ExecutionPolicy RemoteSigned ConfigureRemotingForAnsible.ps1
-Remove-Item -path ConfigureRemotingForAnsible.ps1 -force
