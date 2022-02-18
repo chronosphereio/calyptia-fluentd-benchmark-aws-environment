c6ccac59b86653afb1eec1a3ee1ca0c11533ee07
Hiroshi Hatake <hatake@calyptia.com>
2022-02-18 15:27:51 +0900

    in_tail: fluent-bit: Deploy prometheus as simple systemd service
    Signed-off-by: Hiroshi Hatake <hatake@calyptia.com>

--- in_tail_bench_fluent_bit/network.tf
+++ in_tail_bench_fluent_bit/network.tf
@@ -61,6 +61,13 @@ resource "aws_security_group" "sg" {
   }
 
   ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = -1
     to_port     = -1
     protocol    = "icmp"
